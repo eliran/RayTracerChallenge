26d64c8ddea6803fbd723b622a43dd9f34afe55b
Eliran Ben-Ezra <eliran@gc.io>
2019-04-08 22:50:22 -0400

    initial swift project

---
+++ swift/RayTracerChallenge/RayTracerChallenge/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  RayTracerChallenge
+//
+//  Created by Eliran Ben-Ezra on 2019-04-08.
+//  Copyright © 2019 Threeplay Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
